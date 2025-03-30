53488964c85fe3a69935e022bb17c30e85ed6be9
Kadir Şahin <kadirshn2924@gmail.com>
2025-03-30 16:52:37 +0300

    30-03-2025

--- read-file-write-file/src/main.rs
+++ read-file-write-file/src/main.rs
@@ -1,9 +1,19 @@
-use std::fs::{self, File};
+use std::{fs::{self, File, OpenOptions}, io::Write};
 
 fn main() {
     fs::create_dir("files").expect("files adlı klasör oluşturuldu");
     File::create("files/deneme.txt").expect("Dosya Başarıyla Oluşturuldu");
     fs::write("files/deneme.txt", "Merhaba Kadir!").expect("Metin Başarıyla Eklendi");
     let conent = fs::read_to_string("files/deneme.txt").unwrap();
+    let file_path = "files/deneme2.txt";
+    let mut file = OpenOptions::new()
+    .write(true)
+    .create(true)
+    .append(true)
+    //.truncate(true) // eğer dosya varsa içeriği siler baştan yazar
+    .open(file_path).unwrap();
+    file.write_all("Deneme 2 yazısı".as_bytes()).expect("");
+    let content2 = fs::read_to_string("files/deneme2.txt").unwrap();
     println!("{}", conent);
+    println!("{}",content2);
 }
