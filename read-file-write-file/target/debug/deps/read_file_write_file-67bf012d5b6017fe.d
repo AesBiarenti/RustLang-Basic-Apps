0cb502563164fa187fab65b45aacfacfb6d1cbe0
Kadir Şahin <kadirshn2924@gmail.com>
2025-03-30 15:50:34 +0300

    30-03-2025

---
+++ read-file-write-file/target/debug/deps/read_file_write_file-67bf012d5b6017fe.d
@@ -0,0 +1,5 @@
+/home/kadir/software/rust/read-file-write-file/target/debug/deps/libread_file_write_file-67bf012d5b6017fe.rmeta: src/main.rs
+
+/home/kadir/software/rust/read-file-write-file/target/debug/deps/read_file_write_file-67bf012d5b6017fe.d: src/main.rs
+
+src/main.rs:
