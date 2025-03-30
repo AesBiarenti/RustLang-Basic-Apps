87731de81905def62c62b6a5cc2d4e928c1705e8
Kadir Şahin <kadirshn2924@gmail.com>
2025-03-30 16:33:14 +0300

    30-03-2025

--- read-file-write-file/src/main.rs
+++ read-file-write-file/src/main.rs
@@ -4,6 +4,6 @@ fn main() {
     fs::create_dir("files").expect("files adlı klasör oluşturuldu");
     File::create("files/deneme.txt").expect("Dosya Başarıyla Oluşturuldu");
     fs::write("files/deneme.txt", "Merhaba Kadir!").expect("Metin Başarıyla Eklendi");
-    let conent = fs::read_to_string("files/deneme.txt").expect("Dosya Okuma İşlemi yapılıyor");
+    let conent = fs::read_to_string("files/deneme.txt").unwrap();
     println!("{}", conent);
 }
